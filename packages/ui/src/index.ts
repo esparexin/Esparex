a988fa3e50f4b77dd146c03dc5e2325b68fa48c9
Esparex <esparex.in@gmail.com>
2026-07-24 15:10:20 +0530

    feat(ui): extract canonical Form infrastructure to packages/ui

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,4 +2,10 @@ export * from "./utils";
 export * from "./popup/popupDialog";
 export * from "./popup/popupDialogView";
 export * from "./hooks/usePopupQueue";
+
+// UI Primitives
 export * from "./atoms/Button";
+export * from "./atoms/FormError";
+
+// UI Patterns
+export * from "./patterns/Field";
