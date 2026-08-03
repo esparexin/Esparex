9c0d32d2b213493597487ea754cd265a0a53add6
Esparex <esparex.in@gmail.com>
2026-08-03 18:16:08 +0530

    fix(architecture): add missing domain public barrels and manifests (#PR4B)

---
+++ core/src/domains/boosts/index.ts
@@ -0,0 +1,3 @@
+export * from "./application/services/AdSlotService";
+export * from "./application/services/BoostService";
+export * from "./application/services/PromotionPolicyService";
---
+++ core/src/domains/identity/index.ts
@@ -0,0 +1,7 @@
+export * from "./application/auth/AuthService";
+export * from "./application/users/UserService";
+export * from "./application/users/UserProfileService";
+export * from "./application/users/UserStatusService";
+export * from "./application/users/AdminUsersService";
+export * from "./application/sessions/AdminSessionService";
+export * from "./application/roles/roleNormalization";
