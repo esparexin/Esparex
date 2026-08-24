5c6af0ce1398c863a1020f8a65fdd49a4659c4b5
Esparex <esparex.in@gmail.com>
2026-08-24 16:54:05 +0530

    fix(ui): configure extendTailwindMerge to preserve design token font size classes

--- packages/ui/src/utils.ts
+++ packages/ui/src/utils.ts
@@ -1,6 +1,26 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        "text-display",
+        "text-h1",
+        "text-h2",
+        "text-h3",
+        "text-h4",
+        "text-body-lg",
+        "text-body",
+        "text-small",
+        "text-caption",
+        "text-tiny",
+        "text-2xs"
+      ]
+    }
+  }
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
